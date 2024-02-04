985b70f486018d52b492587869a2fd6ba7209ea5
Ricardo Evz <ricardo.esp.vz@gmail.com>
2024-02-03 20:48:07 -0600

    ADDED: Hello world added in unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, github-actions');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello world, github-actions'
+    );
   });
 });
